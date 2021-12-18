b657c476f69dfcd5fb2b1504d31b6a259134916f
SnkrFr3sh <mpacampara@gmail.com>
2021-12-17 19:41:44 -0800

    adding seeds file

--- schema.sql
+++ schema.sql
@@ -12,7 +12,8 @@ CREATE TABLE role (
     id INT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -20,5 +21,6 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL
-)
+    manager_id INT, 
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+)4
